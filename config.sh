ea0532770df9cc57591538c628d18f13b6226b4b
Kellen Ramos <kellenramosph@gmail.com>
2025-11-26 02:02:58 +0100

    Add autovm (#1)
    * Add manual install
    
    * Add scripts

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Configuration variables
+VM_NAME="debian-xfce-docker"
+VM_MEMORY=4096  # 4GB RAM
+VM_CPUS=2
+VM_DISK_SIZE=30000  # 30GB
+VM_VRAM=128  # Video memory in MB
+
+# Debian ISO configuration
+# https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-13.2.0-amd64-netinst.iso
+DEBIAN_VERSION="13.2.0"
+DEBIAN_ISO_NAME="debian-${DEBIAN_VERSION}-amd64-netinst.iso"
+DEBIAN_ISO_PATH="./iso/$DEBIAN_ISO_NAME"
+DEBIAN_ISO_URL="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/$DEBIAN_ISO_NAME"
+
+SHARED_FOLDER_HOST="$HOME/vm-shared"  # Folder on your host machine
+SHARED_FOLDER_NAME="shared"
---
+++ iso/README.md
@@ -0,0 +1,1 @@
+# Linux CD Iso files
